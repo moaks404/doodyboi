e781b6966d39323f59e30140f9f47fec9afe8c35
trevor <trevor.miller@oroville.wednet.edu>
2019-01-22 15:27:14 -0800

    Initial commit

---
+++ include/chassis.h
@@ -0,0 +1,6 @@
+#ifndef _chassis_H_
+#define _chassis_H_
+
+void driveForDistancePID(int distance, int speed);
+
+#endif
---
+++ src/opcontrol.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include "portdef.h"
+
+
+void operatorControl() {
+  int left_speed = 0; // variabels to hold the speed from joysticks
+  int right_speed = 0;
+
+  while (1) { // read remote control forever
+    // connect the joysticks to the left and right speed varibale to set te set
+    // speed for the motors subsequently
+    right_speed = joystickGetAnalog(1, 2); // vertical axis on right joystick
+    left_speed = joystickGetAnalog(1, 3);  // vertical axis on left joystick
+
+    int lift_speed = 50;
+		if(joystickGetDigital(1,6, JOY_UP)) {
+            motorSet(LEFT_M_ARM, lift_speed);
+            motorSet(RIGHT_M_ARM, lift_speed);
+            printf("in 6 JOY_UP \n");
+    }
+    else if(joystickGetDigital(1,6, JOY_DOWN)) {
+            motorSet(LEFT_M_ARM, -lift_speed);
+            motorSet(RIGHT_M_ARM, -lift_speed);
+    }
+    else if(joystickGetDigital(1,8, JOY_UP)) {
+            motorSet(LEFT_M_ARM, -lift_speed/2);
+            motorSet(RIGHT_M_ARM, -lift_speed/2);
+    }
+    else if(joystickGetDigital(1,8, JOY_DOWN)) {
+        printf("The button group 1-8 pressed\n");
+            motorSet(LEFT_M_ARM, -lift_speed/8);
+            motorSet(RIGHT_M_ARM, -lift_speed/8);
+    }
+    else {
+            motorSet(LEFT_M_ARM, 0);
+            motorSet(RIGHT_M_ARM, 0);
+    }
+    // connect the joystick movement ot he motor groups, we have two left and
+    // two right
+    // motors.  the right motors are mirrored (does reveresed) from the left
+    // motor
+    motorSet(LEFT_M_FRONT, left_speed);    // set left motor speed
+    motorSet(LEFT_M_BACK, left_speed);     // set left motor speed
+    motorSet(RIGHT_M_FRONT, -right_speed); // set right motor speed
+    motorSet(RIGHT_M_BACK, -right_speed);
+    delay(20); // give cortex time to set the motors speed
+  }
+}
